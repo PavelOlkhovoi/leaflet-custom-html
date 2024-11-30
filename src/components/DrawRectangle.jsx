39d2d09b7c88737eb47a045b4b6c9f9fd1196eeb
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-30 21:05:16 +0100

    convert lat lng to pixel

--- src/components/DrawRectangle.jsx
+++ src/components/DrawRectangle.jsx
@@ -16,10 +16,25 @@ const DrawRectangle = () => {
       const newCoord = [...polCoord.current, [lat, lng], polCoord.current[0]];
       const polygon = L.polygon(newCoord, { color: "red" }).addTo(map);
       const getB = polygon.getBounds();
+
+      const { _northEast } = getB;
       console.log("xxx", getB);
+      console.log("xxx _northEast", _northEast.lat, _northEast.lng);
       // zoom the map to the polygon
       map.fitBounds(polygon.getBounds());
-      counter.current = 0;
+
+      const zoom = map.getZoom();
+      const { x, y } = map.latLngToLayerPoint(_northEast);
+      // console.log("xxx north", map.options.crs.latLngToPoint(_northEast, zoom));
+      console.log("xxx north", x, y);
+
+      const popa = document.getElementById("popa");
+      popa.style.left = x + "px";
+      popa.style.top = y + "px";
+
+      console.log("xxx popa", popa);
+
+      popa.style.counter.current = 0;
       polCoord.current = [];
     }
   };
@@ -27,13 +42,20 @@ const DrawRectangle = () => {
     console.log("xxx");
     if (routedMapRef) {
       const map = routedMapRef.leafletMap.leafletElement;
-
       map.on("click", (e) => {
+        console.log("xxx click", e);
         handlerClick(e, map);
       });
     }
   }, [routedMapRef, counter]);
-  return <div>Test</div>;
+  return (
+    <div
+      id="popa"
+      style={{ position: "absolute", zIndex: 10000, left: "0", top: "0" }}
+    >
+      Test
+    </div>
+  );
 };
 
 export default DrawRectangle;
