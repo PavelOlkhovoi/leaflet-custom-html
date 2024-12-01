8fe7e356bbe4a7f14ffea48e1df31533296c3254
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-12-01 11:22:22 +0100

    add helper

--- src/components/DrawRectangle.jsx
+++ src/components/DrawRectangle.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { TopicMapContext } from "react-cismap/contexts/TopicMapContextProvider";
+import { changeLefTopPos } from "../helper/rectHelper";
 
 const DrawRectangle = () => {
   const { routedMapRef } = useContext(TopicMapContext);
@@ -11,6 +12,8 @@ const DrawRectangle = () => {
     const { lat, lng } = e.latlng;
     const coord = [...polCoord.current, [lat, lng]];
     polCoord.current = coord;
+    console.log("xxx ...", e.layerPoint.x, e.layerPoint.y);
+    // changeLefTopPos(e.layerPoint.x, e.layerPoint.y);
     counter.current = counter.current + 1;
     if (counter.current === 4) {
       const newCoord = [...polCoord.current, [lat, lng], polCoord.current[0]];
@@ -21,20 +24,15 @@ const DrawRectangle = () => {
       console.log("xxx", getB);
       console.log("xxx _northEast", _northEast.lat, _northEast.lng);
       // zoom the map to the polygon
-      map.fitBounds(polygon.getBounds());
+      // map.fitBounds(polygon.getBounds());
 
       const zoom = map.getZoom();
-      const { x, y } = map.latLngToLayerPoint(_northEast);
+      const { x, y } = map.latLngToLayerPoint(_northEast, zoom);
       // console.log("xxx north", map.options.crs.latLngToPoint(_northEast, zoom));
       console.log("xxx north", x, y);
+      changeLefTopPos(x, y);
 
-      const popa = document.getElementById("popa");
-      popa.style.left = x + "px";
-      popa.style.top = y + "px";
-
-      console.log("xxx popa", popa);
-
-      popa.style.counter.current = 0;
+      counter.current = 0;
       polCoord.current = [];
     }
   };
@@ -51,7 +49,15 @@ const DrawRectangle = () => {
   return (
     <div
       id="popa"
-      style={{ position: "absolute", zIndex: 10000, left: "0", top: "0" }}
+      style={{
+        position: "absolute",
+        zIndex: 10000,
+        // left: "0",
+        top: "0",
+        // right: "600",
+        // bottom: "400",
+        background: "red",
+      }}
     >
       Test
     </div>
---
+++ src/helper/rectHelper.js
@@ -0,0 +1,5 @@
+export const changeLefTopPos = (x, y) => {
+  const popa = document.getElementById("popa");
+  popa.style.left = x + "px";
+  popa.style.top = y + "px";
+};
